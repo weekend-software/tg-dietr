c731ec7cb986607d35c8d194dccc8b3a73ed9271
Kirill K <agrrh@users.noreply.github.com>
2022-08-24 22:59:36 +0300

    feat: add data-api (#5)

---
+++ data-api/helpers/user.py
@@ -0,0 +1,99 @@
+from influxdb_client import Point
+
+from helpers.base import BaseHelper
+
+from models.user import User
+
+
+class UserHelper(BaseHelper):
+    MEASUREMENT_NAME = "users"
+
+    def exists(self, id):
+        q = """
+        from(bucket: pBucket)
+            |> range(start: 0)
+            |> filter(fn: (r) => r._measurement == pMeasurement)
+            |> filter(fn: (r) => r.id == pId)
+            |> count()
+        """
+
+        p = {
+            "pBucket": self.bucket,
+            "pMeasurement": self.MEASUREMENT_NAME,
+            "pId": str(id),
+        }
+
+        tables = self.query_api.query(query=q, params=p)
+
+        for table in tables:
+            for record in table.records:
+                if record.values.get("id") == str(id):
+                    return True
+
+        return False
+
+    def get(self, id):
+        q = """
+        from(bucket: pBucket)
+            |> range(start: 0)
+            |> filter(fn: (r) => r._measurement == pMeasurement)
+            |> filter(fn: (r) => r.id == pId)
+            |> last()
+            |> group()
+        """
+
+        p = {
+            "pBucket": self.bucket,
+            "pMeasurement": self.MEASUREMENT_NAME,
+            "pId": str(id),
+        }
+
+        tables = self.query_api.query(query=q, params=p)
+
+        for table in tables:
+            for record in table.records:
+                if record.values.get("_field") == "exists":
+                    created_at = record.values.get("_time")
+
+                if record.values.get("_field") == "activation":
+                    activated_at = record.values.get("_time")
+
+                if record.values.get("_field") == "deactivation":
+                    deactivated_at = record.values.get("_time")
+
+        return User(id=id, created_at=created_at, activated_at=activated_at, deactivated_at=deactivated_at)
+
+    def list(self):
+        q = """
+        from(bucket: pBucket)
+            |> range(start: 0)
+            |> filter(fn: (r) => r._measurement == pMeasurement)
+            |> unique(column: "id")
+            |> keep(columns: ["id"])
+        """
+
+        p = {
+            "pBucket": self.bucket,
+            "pMeasurement": self.MEASUREMENT_NAME,
+        }
+
+        tables = self.query_api.query(query=q, params=p)
+
+        for table in tables:
+            for record in table.records:
+                yield record.values.get("id")
+
+    def create_and_activate(self, id):
+        return self.create(id) or self.activate(id)
+
+    def create(self, id):
+        p = Point(self.MEASUREMENT_NAME).tag("id", id).field("exists", 1)
+        return self.write_api.write(bucket=self.bucket, record=p)
+
+    def activate(self, id):
+        p = Point(self.MEASUREMENT_NAME).tag("id", id).field("activation", 1)
+        return self.write_api.write(bucket=self.bucket, record=p)
+
+    def deactivate(self, id):
+        p = Point(self.MEASUREMENT_NAME).tag("id", id).field("deactivation", 1)
+        return self.write_api.write(bucket=self.bucket, record=p)
---
+++ data-api/main.py
@@ -0,0 +1,128 @@
+import logging
+import os
+import sys
+import yaml
+
+from fastapi import FastAPI, HTTPException
+
+from models.user import User
+from models.metric import Metric
+
+from helpers.user import UserHelper
+from helpers.metric import MetricHelper
+
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+db_config_raw = os.environ.get("APP_DB_CONFIG")
+try:
+    db_config = yaml.load(db_config_raw, Loader=yaml.BaseLoader)
+except Exception as e:
+    logging.exception(f"Could not load config: {e}")
+    sys.exit(1)
+
+db_bucket = os.environ.get("APP_DB_BUCKET")
+
+client = InfluxDBClient(**db_config)
+
+db_write_api = client.write_api(write_options=SYNCHRONOUS)
+db_query_api = client.query_api()
+
+user_helper = UserHelper(query_api=db_query_api, write_api=db_write_api, bucket=db_bucket)
+metric_helper = MetricHelper(query_api=db_query_api, write_api=db_write_api, bucket=db_bucket)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"detail": "Hello World"}
+
+
+@app.get("/health")
+async def health():
+    # TODO Implement
+    return {"detail": "Hello World"}
+
+
+@app.post("/users/register")
+async def users_register(user: User):
+    user_exists = user_helper.exists(user.id)
+
+    if user_exists:
+        raise HTTPException(status_code=409, detail="User already exists")
+
+    user_helper.create_and_activate(id=user.id)
+
+    return {
+        "detail": "User created",
+        # TODO Read user back and return object
+        "user": user,
+    }
+
+
+@app.get("/users/")
+async def users_list():
+    users = user_helper.list()
+
+    # TODO Get count from DB
+    users_count = len(list(users))
+
+    return {"count": users_count, "users": users}
+
+
+@app.get("/users/{user_id}")
+async def users_get(user_id: int):
+    user = user_helper.get(id=user_id)
+
+    return {
+        "user": user,
+    }
+
+
+@app.patch("/users/{user_id}/activate")
+async def users_activate(user_id: int):
+    user_helper.activate(id=user_id)
+
+    user = user_helper.get(id=user_id)
+
+    return {
+        "detail": "User activated",
+        "user": user,
+    }
+
+
+@app.patch("/users/{user_id}/deactivate")
+async def users_deactivate(user_id: int):
+    user_helper.deactivate(id=user_id)
+
+    user = user_helper.get(id=user_id)
+
+    return {
+        "detail": "User deactivated",
+        "user": user,
+    }
+
+
+@app.post("/users/{user_id}/metrics/")
+async def users_add_metric(user_id: int, metric: Metric):
+    metric_helper.write(user_id=user_id, metric=metric)
+
+    return {
+        "detail": "Metric added",
+    }
+
+
+@app.get("/users/{user_id}/metrics/")
+async def users_list_metrics(user_id: int):
+    metrics = metric_helper.list(user_id)
+    metrics_count = len(metrics.keys())
+
+    return {"count": metrics_count, "metrics": metrics}
+
+
+@app.get("/users/{user_id}/metrics/{metric_name}")
+async def users_get_metric_values(user_id: int, metric_name: str, limit: int = 30, offset: int = 0):
+    values = metric_helper.get_values(user_id, metric_name, offset=offset, limit=limit)
+
+    return {"user_id": user_id, "metric_name": metric_name, "values": values}
