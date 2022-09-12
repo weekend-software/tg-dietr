73abb0d22b444c33bc0adb44d9941ae402c651b5
Stsochka <stsochka007@gmail.com>
2022-09-12 11:51:41 +0300

    feat: send massage to telegram

--- reminder/main.py
+++ reminder/main.py
@@ -1,12 +1,21 @@
 import schedule
 import time
 import os
+import telebot
 
 DEV_MODE = os.environ.get("APP_DEV_MODE")
 
+API_TOKEN = os.environ.get("APP_TELEGRAM_TOKEN")
+
+bot = telebot.TeleBot(API_TOKEN)
+
+user_id = 187281514
 
 def job():
-    print("I'm working...")
+    bot.send_message(
+        user_id,
+        "Внесите ваши данные",
+    )
 
 if not DEV_MODE:
     schedule.every().saturday.at("12:00").do(job)
